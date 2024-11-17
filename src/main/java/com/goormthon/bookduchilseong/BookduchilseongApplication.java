fc983ea6f9fa0b6e9fe88cf2839c47746068dc3e
MJJ <m3k0813@gmail.com>
2024-11-18 00:06:45 +0900

    chore: 스프링 초기 세팅

---
+++ src/main/java/com/goormthon/bookduchilseong/BookduchilseongApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.bookduchilseong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookduchilseongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookduchilseongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthon/bookduchilseong/BookduchilseongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.bookduchilseong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookduchilseongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
