0eb130cd3d0d663cf64c7b28c828e89dfbbe368e
MJJ <m3k0813@gmail.com>
2024-11-18 16:20:24 +0900

    refactor: UserBookClub @Table 추가

--- src/main/java/com/goormthon/bookduchilseong/domain/userbookclub/entity/UserBookClub.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/userbookclub/entity/UserBookClub.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor()
 @NoArgsConstructor
+@Table(name = "user_book_club")
 public class UserBookClub {
 
 	@Id
