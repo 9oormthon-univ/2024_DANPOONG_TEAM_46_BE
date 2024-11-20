2687c7486dcef8edd42dec1a2fac16335994c656
Your Name <kjeng7897@gmail.com>
2024-11-21 01:27:50 +0900

    feat:특정 사용자가 참여중인 북클럽 목록 가져오기

--- src/main/java/com/goormthon/bookduchilseong/domain/user/dto/response/UserMyPageReseponseDto.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/dto/response/UserMyPageReseponseDto.java
@@ -1,2 +1,27 @@
-package com.goormthon.bookduchilseong.domain.user.dto.response;public class UserMyPageReseponseDto {
+package com.goormthon.bookduchilseong.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class UserMyPageReseponseDto {
+    private int zodiacsignCnt; // 별자리 개수
+    private int readingCnt;    // 독서 개수
+    private int bookClubCnt;   // 책 모임 개수
+    private List<MyBook> myBook; // 책 정보 리스트
+
+    @Getter
+    @AllArgsConstructor
+    public static class MyBook{
+        private String title;   // 책 제몬
+        private String author;  // 저자
+        private int startPoint; // 시작 페이지
+        private int totalPage;  // 전체 페이지
+        private int readPage;   // 현재 읽은 페이지
+        private int progress;   // 진행도(퍼센트)
+    }
+
 }
--- src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserServiceImpl.java
@@ -1,18 +1,31 @@
 package com.goormthon.bookduchilseong.domain.user.service;
 
+import com.goormthon.bookduchilseong.domain.book.entity.Book;
 import com.goormthon.bookduchilseong.domain.book.repository.BookRepository;
+import com.goormthon.bookduchilseong.domain.user.dto.response.UserMyPageReseponseDto;
+import com.goormthon.bookduchilseong.domain.user.entity.User;
 import com.goormthon.bookduchilseong.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserServiceImpl {
+
     private final UserRepository userRepository;
     private final BookRepository bookRepository;
-    public String createUser(Authentication authentication){
-        //JWT로부터 사용자 정보 가져오기
-        
+    UserMyPageReseponseDto getUserMypage(Long userId){
+        // 유저 조회
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
+
+        // 서재 정보 가져오기
+        List<Book> books = bookRepository.findBooksByUserId(userId);
+
+        // 마이페이지 응답 생성
+
     }
 }
