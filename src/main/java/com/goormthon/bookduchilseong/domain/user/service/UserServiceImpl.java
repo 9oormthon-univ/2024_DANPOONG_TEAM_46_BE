a9da956b667f338063a3d0c6b49119edcec84a00
Your Name <kjeng7897@gmail.com>
2024-11-21 03:03:13 +0900

    refactor:RedisConfig

--- src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserServiceImpl.java
@@ -10,30 +10,30 @@ import com.goormthon.bookduchilseong.domain.user.repository.UserRepository;
 import com.goormthon.bookduchilseong.domain.zodiacsign.entity.Zodiacsign;
 import com.goormthon.bookduchilseong.domain.zodiacsign.repository.ZodiacsignRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final BookRepository bookRepository;
     private final ZodiacsignRepository zodiacsignRepository;
     private final BookClubRepository bookClubRepository;
-    UserMyPageReseponseDto getUserMypage(Long userId){
+
+    @Override
+    public UserMyPageReseponseDto getUserMypage(Long userId) {
         // 유저 조회
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
-        // 유저 ID로 연관된 별자리 데이터 가져오기
+        // 연관 데이터 가져오기
         List<Zodiacsign> zodiacsigns = zodiacsignRepository.findByUser(user);
-        // 서재 정보 가져오기
         List<Book> books = bookRepository.findBooksByUserId(userId);
-        // 특정 사용자가 참여 중인 북클럽 목록 가져오기
         List<BookClub> bookClubs = bookClubRepository.findByUser(user);
+
         // 책 리스트를 myBook 형식으로 매핑
         List<UserMyPageReseponseDto.MyBook> myBooks = books.stream()
                 .map(book -> new UserMyPageReseponseDto.MyBook(
@@ -52,11 +52,9 @@ public class UserServiceImpl {
                 bookClubs.size(),
                 myBooks
         );
-
-    }
-    // 책의 진행도를 계산하는 메서드
-    private int calculateProgress(int startPoint, int totalPage){
-        return (int) ((startPoint/totalPage) * 100);
     }
 
+    private int calculateProgress(int startPoint, int totalPage) {
+        return (int) ((startPoint / (double) totalPage) * 100);
+    }
 }
