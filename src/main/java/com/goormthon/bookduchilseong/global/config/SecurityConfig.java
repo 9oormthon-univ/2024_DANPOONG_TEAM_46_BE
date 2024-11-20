a3bdfe83233d080e7c97e99a4e7da846678d26e7
Your Name <kjeng7897@gmail.com>
2024-11-20 22:18:55 +0900

    feat:JWTToken 관련 기능 개발

--- src/main/java/com/goormthon/bookduchilseong/global/config/SecurityConfig.java
+++ src/main/java/com/goormthon/bookduchilseong/global/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.goormthon.bookduchilseong.global.config;
 
-// global/config/SecurityConfig.java
+import com.goormthon.bookduchilseong.global.security.jwt.JwtAuthenticationFilter;
+import com.goormthon.bookduchilseong.global.security.jwt.JwtTokenProvider;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,53 +23,69 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) // @PreAuthorize 활성화
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthorizationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .cors(httpSecurityCorsConfigurer ->
                         httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource())
                 )
                 .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
                 .formLogin(AbstractHttpConfigurer::disable) // 기본 login form 비활성화
                 .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화(쿠키 사용 안해서 불필요)
-                .oauth2Login(Customizer.withDefaults()) //oauth2
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 미사용
 
-
         // Public endpoint 허용
-        http.authorizeHttpRequests(auth ->
-                auth
-                        .requestMatchers("/", "/login", "/oauth2/**").permitAll()
-                        .anyRequest().authenticated()
+        httpSecurity.authorizeHttpRequests(authorizeHttpRequests ->
+                authorizeHttpRequests
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers("http://localhost:8080/api/v1/oauth/kakao/callback").permitAll() // 인증 없이 허용
                         .requestMatchers("/api/v1/account/signup").permitAll()
                         .requestMatchers("/api/v1/auth/login").permitAll()
                         .requestMatchers("/api/v1/auth/token/refresh").permitAll()
                         .requestMatchers("/api/v1/oauth/**").permitAll()
                         .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/**").permitAll() // TODO - 수정
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
         );
 
-        http.oauth2Login(oauth ->
-                oauth.userInfoEndpoint()
-                        .userService(customOAuth2UserService()) // 사용자 정보 처리 서비스 등록
+        // JWT 필터 추가
+        httpSecurity.addFilterBefore(
+                jwtAuthorizationFilter(),
+                UsernamePasswordAuthenticationFilter.class
         );
 
-        return http.build();
+        return httpSecurity.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // TODO - frontend 배포 도메인으로 변경
-        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("https://your-frontend-domain.com")); // TODO: 프론트엔드 도메인으로 변경
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedMethods(
---
+++ src/main/java/com/goormthon/bookduchilseong/global/config/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.goormthon.bookduchilseong.global.config;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
