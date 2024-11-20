4304cc0f5ebb8fab55abe776be7c092010f40f8a
MJJ <m3k0813@gmail.com>
2024-11-21 00:44:48 +0900

    refactor: 도서 읽기 완료 시 draw 추가

--- src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationServiceImpl.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationServiceImpl.java
@@ -2,16 +2,19 @@ package com.goormthon.bookduchilseong.domain.certification.service;
 
 import org.springframework.stereotype.Service;
 
+import com.goormthon.bookduchilseong.domain.user.entity.User;
 import com.goormthon.bookduchilseong.domain.book.entity.Book;
 import com.goormthon.bookduchilseong.domain.book.repository.BookRepository;
 import com.goormthon.bookduchilseong.domain.certification.dto.request.CertificationRequestDTO;
 import com.goormthon.bookduchilseong.domain.certification.entity.Certification;
 import com.goormthon.bookduchilseong.domain.certification.repository.CertificationRepository;
+import com.goormthon.bookduchilseong.domain.user.repository.UserRepository;
 import com.goormthon.bookduchilseong.global.apiPayload.ApiResponse;
 import com.goormthon.bookduchilseong.global.apiPayload.code.status.ErrorStatus;
 import com.goormthon.bookduchilseong.global.apiPayload.exception.GeneralException;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -19,30 +22,56 @@ public class CertificationServiceImpl implements CertificationService {
 
 	private final CertificationRepository certificationRepository;
 	private final BookRepository bookRepository;
+	private final UserRepository userRepository;
 
 	@Override
-	public ApiResponse<String> createCertification(Long bookId, CertificationRequestDTO requestDto) {
+	public ApiResponse<String> createCertification(Long userId, Long bookId, CertificationRequestDTO requestDTO) {
 		// Certification 엔티티 생성 및 저장
 		Certification certification = Certification.builder()
 			.book(findBookById(bookId))
-			.startPage(requestDto.getStartPage())
-			.endPage(requestDto.getEndPage())
-			.image(requestDto.getImage())
-			.paragraph(requestDto.getParagraph())
+			.startPage(requestDTO.getStartPage())
+			.endPage(requestDTO.getEndPage())
+			.image(requestDTO.getImage())
+			.paragraph(requestDTO.getParagraph())
 			.build();
 
 		certificationRepository.save(certification);
 
 		Book book = findBookById(bookId);
-		book.updateReadPage(requestDto.getEndPage());
+		book.updateReadPage(requestDTO.getEndPage());
 		bookRepository.save(book);
+
+		if (isCompleteBook(bookId)) {
+			addDraw(userId);
+		}
 
 		// 성공 응답 반환
 		return ApiResponse.onSuccess("도서 인증하기 성공");
 	}
 
+	private User findUserById(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> new RuntimeException("해당 사용자를 찾을 수 없습니다."));
+	}
+
 	private Book findBookById(Long bookId) {
 		return bookRepository.findById(bookId)
 			.orElseThrow(() -> new GeneralException(ErrorStatus._BOOK_NOT_FOUND));
+	}
+
+	private boolean isCompleteBook(Long bookId) {
+
+		Book book = findBookById(bookId);
+
+		if (book.getReadPage().equals(book.getTotalPage())) {
+			return true;
+		}
+		return false;
+	}
+
+	private void addDraw(Long userId) {
+		User user = findUserById(userId);
+		user.addDraw();
+		userRepository.save(user);
 	}
 }
