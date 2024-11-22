a67c61b283653b17d69ad63d4d1f2328badb99b3
Your Name <kjeng7897@gmail.com>
2024-11-22 13:58:19 +0900

    feat: 카카오 사용자 정보 responseDto

--- src/main/java/com/goormthon/bookduchilseong/global/auth/dto/response/KakaoUserResponseDto.java
+++ src/main/java/com/goormthon/bookduchilseong/global/auth/dto/response/KakaoUserResponseDto.java
@@ -1,14 +1,34 @@
-package com.goormthon.bookduchilseong.global.auth.dto;
+package com.goormthon.bookduchilseong.global.auth.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class KakaoUserResponseDto {
-    private long id;
-    private String nickname;
-    private String thumbnailImageUrl;
+public record KakaoUserResponseDto(
+        Long id,
+        @JsonProperty("connected_at") String connectedAt,
+        Properties properties,
+        @JsonProperty("kakao_account") KakaoAccount kakaoAccount
+) {
+    public record Properties(
+            String nickname,
+            @JsonProperty("profile_image") String profileImage,
+            @JsonProperty("thumbnail_image") String thumbnailImage
+    ) {}
+
+    public record KakaoAccount(
+            @JsonProperty("email_needs_agreement") Boolean emailNeedsAgreement,
+            @JsonProperty("is_email_valid") Boolean isEmailValid,
+            @JsonProperty("is_email_verified") Boolean isEmailVerified,
+            String email,
+            @JsonProperty("profile_nickname_needs_agreement") Boolean profileNicknameNeedsAgreement,
+            @JsonProperty("profile_image_needs_agreement") Boolean profileImageNeedsAgreement,
+            Profile profile
+    ) {
+        public record Profile(
+                String nickname,
+                @JsonProperty("thumbnail_image_url") String thumbnailImageUrl,
+                @JsonProperty("profile_image_url") String profileImageUrl,
+                @JsonProperty("is_default_image") Boolean isDefaultImage,
+                @JsonProperty("is_default_nickname") Boolean isDefaultNickname
+        ) {}
+    }
 }
