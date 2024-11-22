2caf3c9b2e131682109fab561525975691706d1f
Your Name <kjeng7897@gmail.com>
2024-11-22 16:51:38 +0900

    refactor: UserService parameter 수정

--- src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserService.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/service/UserService.java
@@ -1,9 +1,11 @@
     package com.goormthon.bookduchilseong.domain.user.service;
 
     import com.goormthon.bookduchilseong.domain.user.dto.response.UserMyPageReseponseDto;
-    import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+    import com.goormthon.bookduchilseong.domain.user.entity.User;
+    import com.goormthon.bookduchilseong.global.auth.dto.response.KakaoUserResponseDto;
 
     public interface UserService {
+        User findOrCreateUser(KakaoUserResponseDto kakaoUser);
         // 마이페이지 조회
         UserMyPageReseponseDto getUserMypage(Long userId);
     }
