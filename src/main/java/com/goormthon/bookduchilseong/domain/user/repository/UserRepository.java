23cf31a7c6cfa0b942d65dfd2c87519d9bad8588
Your Name <kjeng7897@gmail.com>
2024-11-22 15:58:26 +0900

    refactor: UserRepository

--- src/main/java/com/goormthon/bookduchilseong/domain/user/repository/UserRepository.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/repository/UserRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.goormthon.bookduchilseong.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
 }
