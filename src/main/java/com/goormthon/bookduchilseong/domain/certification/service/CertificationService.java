722e1d55e794d1ca486221f7b59ea1d0b4c6259b
MJJ <m3k0813@gmail.com>
2024-11-24 00:54:31 +0900

    feat: 인증 이미지 요약하기

--- src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationService.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationService.java
@@ -1,8 +1,12 @@
 package com.goormthon.bookduchilseong.domain.certification.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.goormthon.bookduchilseong.domain.certification.dto.request.CertificationRequestDTO;
 import com.goormthon.bookduchilseong.global.apiPayload.ApiResponse;
 
 public interface CertificationService {
 	ApiResponse<String> createCertification(Long userId, Long bookId, CertificationRequestDTO requestDto);
+
+	String createSummation(Long certficationId, MultipartFile image);
 }
--- src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationServiceImpl.java
+++ src/main/java/com/goormthon/bookduchilseong/domain/certification/service/CertificationServiceImpl.java
@@ -1,12 +1,15 @@
 package com.goormthon.bookduchilseong.domain.certification.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.goormthon.bookduchilseong.domain.book.entity.Book;
 import com.goormthon.bookduchilseong.domain.book.repository.BookRepository;
 import com.goormthon.bookduchilseong.domain.certification.dto.request.CertificationRequestDTO;
 import com.goormthon.bookduchilseong.domain.certification.entity.Certification;
 import com.goormthon.bookduchilseong.domain.certification.repository.CertificationRepository;
+import com.goormthon.bookduchilseong.domain.gemini.service.GeminiService;
+import com.goormthon.bookduchilseong.domain.ocr.service.OCRService;
 import com.goormthon.bookduchilseong.domain.user.entity.User;
 import com.goormthon.bookduchilseong.domain.user.repository.UserRepository;
 import com.goormthon.bookduchilseong.global.apiPayload.ApiResponse;
@@ -22,6 +25,8 @@ public class CertificationServiceImpl implements CertificationService {
 	private final CertificationRepository certificationRepository;
 	private final BookRepository bookRepository;
 	private final UserRepository userRepository;
+	private final OCRService ocrService;
+	private final GeminiService geminiService;
 
 	@Override
 	public ApiResponse<String> createCertification(Long userId, Long bookId, CertificationRequestDTO requestDto) {
@@ -48,14 +53,43 @@ public class CertificationServiceImpl implements CertificationService {
 		return ApiResponse.onSuccess("도서 인증하기 성공");
 	}
 
+	@Override
+	public String createSummation(Long certficationId, MultipartFile image) {
+
+		Certification certification = findCertificationById(certficationId);
+
+		String imageText = ocrService.execute(image);
+
+		if (imageText == null) {
+			throw new GeneralException(ErrorStatus._IMAGE_TEXT_NOT_CREATE);
+		}
+
+		String summation = geminiService.getContents(imageText);
+
+		if (summation == null) {
+			throw new GeneralException(ErrorStatus._SUMMATION_NOT_CREATE);
+		}
+
+		certification.createSummation(summation);
+
+		certificationRepository.save(certification);
+
+		return summation;
+	}
+
 	private User findUserById(Long userId) {
 		return userRepository.findById(userId)
-			.orElseThrow(() -> new RuntimeException("해당 사용자를 찾을 수 없습니다."));
+			.orElseThrow(() -> new GeneralException(ErrorStatus._USER_NOT_FOUND));
 	}
 
 	private Book findBookById(Long bookId) {
 		return bookRepository.findById(bookId)
 			.orElseThrow(() -> new GeneralException(ErrorStatus._BOOK_NOT_FOUND));
+	}
+
+	private Certification findCertificationById(Long certficationId) {
+		return certificationRepository.findById(certficationId)
+			.orElseThrow(() -> new GeneralException(ErrorStatus._CERTIFICATION_NOT_FOUND));
 	}
 
 	private boolean isCompleteBook(Long bookId) {
