8d4c8f47bde69bd60e2a27322c3a2fe96c1e1674
MJJ <m3k0813@gmail.com>
2024-11-24 05:59:56 +0900

    feat: 내 뽑기 조회

---
+++ src/main/java/com/goormthon/bookduchilseong/domain/user/dto/response/UserDrawResponseDTO.java
@@ -0,0 +1,14 @@
+package com.goormthon.bookduchilseong.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDrawResponseDTO {
+	private int drawCount;
+}
