47232516559f5f21dfb78f75a37e41c6d1ae1dbd
MJJ <m3k0813@gmail.com>
2024-11-23 21:42:33 +0900

    refactor: swagger jwt 헤더

--- src/main/java/com/goormthon/bookduchilseong/global/config/SwaggerConfig.java
+++ src/main/java/com/goormthon/bookduchilseong/global/config/SwaggerConfig.java
@@ -1,11 +1,12 @@
 package com.goormthon.bookduchilseong.global.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,14 +15,23 @@ public class SwaggerConfig {
 	public OpenAPI openAPI() {
 		return new OpenAPI()
 			.openapi("3.0.0")
-			.components(new Components())
-			.info(apiInfo());
+			.components(new Components().addSecuritySchemes("bearerAuth", createBearerAuth()))
+			.info(apiInfo())
+			.addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+	}
+
+	private SecurityScheme createBearerAuth() {
+		return new SecurityScheme()
+			.name("Authorization")
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT");
 	}
 
 	private Info apiInfo() {
 		return new Info()
 			.title("북두칠성 API")
-			.description("북두칠성 api")
+			.description("북두칠성 API 설명")
 			.version("1.0.0");
 	}
 }
